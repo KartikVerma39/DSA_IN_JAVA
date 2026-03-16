1c401d2765fb3cf2e0b9eeb503e6cdca6cf684e5
KartikVerma39 <kartikverma1074@gmail.com>
2026-03-16 11:26:53 +0530

    DSA_IN_JAVA

---
+++ DSA/Strings/src/MaximumDepthOfParenthesis.java
@@ -0,0 +1,18 @@
+public class MaximumDepthOfParenthesis {
+    public static void main(String[] args){
+        String s = "(1)+((2))+(((3)))";
+        int maxDepth = 0;
+        int depth = 0;
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i) == '('){
+                depth++;
+                maxDepth = Math.max(depth,maxDepth);
+            }
+            else if(s.charAt(i) == ')'){
+                depth--;
+            }
+        }
+
+        System.out.println(maxDepth);
+    }
+}
