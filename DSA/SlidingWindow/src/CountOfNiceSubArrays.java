bfa7d3e9747057ad3291085693e48b9220e260cf
KartikVerma39 <kartikverma1074@gmail.com>
2026-03-24 15:09:54 +0530

    DSA_IN_JAVA

---
+++ DSA/SlidingWindow/src/CountOfNiceSubArrays.java
@@ -0,0 +1,33 @@
+public class CountOfNiceSubArrays {
+    public static void main(String[] args){
+        int[] nums = {1,1,2,1,1};
+        int k = 3;
+        System.out.println(atMost(nums , k) - atMost(nums , k - 1));
+    }
+
+    public static int atMost(int[] nums , int k){
+        int i = 0;
+        int j = 0;
+        int total = 0;
+        int odds = 0;
+        int n = nums.length;
+
+        while(i < n){
+            if(nums[i]%2 != 0){
+                odds++;
+            }
+
+            while(odds > k){
+                if(nums[j]%2 != 0){
+                    odds--;
+                }
+                j++;
+            }
+
+            total += i - j + 1;
+            i++;
+        }
+
+        return total;
+    }
+}
