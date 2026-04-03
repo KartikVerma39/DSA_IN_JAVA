f4a8ff18f0ccdf200db01f48b5dbbeeae5571655
KartikVerma39 <kartikverma1074@gmail.com>
2026-04-03 12:35:26 +0530

    DSA_IN_JAVA

--- DSA/Bit Manipulation/src/Swap2Numbers.java
+++ DSA/Bit Manipulation/src/Swap2Numbers.java
@@ -1,10 +1,11 @@
 public class Swap2Numbers {
     public static void main(String[] args){
-        int a = 5;
-        int b = 3;
+        int a = 10;
+        int b = 5;
         a = a ^ b;
         b = a ^ b;
         a = a ^ b;
+
         System.out.println(a);
         System.out.println(b);
     }
