4c59fb0154aa5ce33c35b5a440d783ea892f79cd
KartikVerma39 <kartikverma1074@gmail.com>
2026-04-03 13:25:58 +0530

    DSA_IN_JAVA

--- DSA/Bit Manipulation/src/RemoveTheLastSetBit.java
+++ DSA/Bit Manipulation/src/RemoveTheLastSetBit.java
@@ -1,6 +1,6 @@
 public class RemoveTheLastSetBit {
     public static void main(String[] args){
-        int n = 40;
-        System.out.println(n & n - 1);
+        int n = 39;
+        System.out.println((n) & (n - 1));
     }
 }
--- DSA/Bit Manipulation/src/ToggleTheIthBit.java
+++ DSA/Bit Manipulation/src/ToggleTheIthBit.java
@@ -2,7 +2,8 @@ public class ToggleTheIthBit {
     public static void main(String[] args){
         int n = 9;
         int i = 2;
-        n = (n ^ (1 << i));
-        System.out.println(n);
+
+        int toggle = (n ^ (1 << i));
+        System.out.println(toggle);
     }
 }
