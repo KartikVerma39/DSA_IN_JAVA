754862fa0b4613d6ce4d9aae323cb0ccc3811874
KartikVerma39 <kartikverma1074@gmail.com>
2026-04-17 12:30:50 +0530

    DSA_IN_JAVA

---
+++ DSA/Strings/src/ReverseAString.java
@@ -0,0 +1,7 @@
+public class ReverseAString {
+    public static void main(String[] args){
+        String s = "KartiK";
+        String ans = new StringBuilder(s).reverse().toString();
+        System.out.println(ans);
+    }
+}
